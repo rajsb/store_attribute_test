247479446a845a3b880846b46ca349239ee298e2
Rajkumar Balakrishnan <thisisrajsb@gmail.com>
2024-11-25 13:44:53 +0530

    Annotate

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: shops
+#
+#  id         :bigint           not null, primary key
+#  settings   :jsonb            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Shop < ApplicationRecord
   store_attribute :settings, :send_welcome_email, :boolean, default: true
   store_attribute :settings, :send_deals_newsletter, :boolean, default: true
